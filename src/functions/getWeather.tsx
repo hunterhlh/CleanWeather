69c03f862fe974bd4b45f490d12d6c151aae4388
hunterhlh <hunterhaislip@gmail.com>
2026-05-28 20:41:24 -0500

    removed api

--- src/functions/getWeather.tsx
+++ src/functions/getWeather.tsx
@@ -1,6 +1,6 @@
 export const fetchWeather = async (latitude: string, longitude: string) => {
     const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=62204fdc004a9f8fab3bdeb0a0ee510b&units=imperial`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=&units=imperial`
     );
     return await response.json();
 };
